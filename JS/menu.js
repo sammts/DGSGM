be47850f7401d0836895b75f429892c6347a94e8
sammts <samueltapia@comunidad.unam.mx>
2023-01-26 14:53:15 -0600

    Busqueda

--- JS/menu.js
+++ JS/menu.js
@@ -40,3 +40,43 @@ cerrar.addEventListener("click", function(){
 window.addEventListener("click", function(e){
     console.log(e.target)
 })
+
+
+
+
+
+
+
+//Barra de búsqueda
+
+bars_search = document.getElementById("cnt-bars-search");
+inputSearch = document.getElementById("inputSearch");
+box_Search = document.getElementById("box-search");
+
+//Mostrar opciones de buscador
+
+document.getElementById("inputSearch").addEventListener("keyup", buscador_interno);
+
+function buscador_interno () {
+    filter = inputSearch.value.toUpperCase();
+    li = box_Search.getElementsByTagName("li");
+
+    for(i=0; i<li.length; i++)
+    {
+        a= li[i].getElementsByTagName("a")[0];
+        textValue = a.textContent || a.innerText;
+
+        if(textValue.toUpperCase().indexOf(filter) > -1)
+        {
+            li[i].style.display = "";
+            box_Search.style.display = "block";
+
+            if(inputSearch.value === ""){
+                box_Search.style.display = "none"
+            }
+
+        }else {
+            li[i].style.display = "none";
+        }
+    }
+}
